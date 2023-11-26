b0a781be1206ae03375cfafd9c234dd8a0c01ed7
tamiryup <tamirishaysh@gmail.com>
2023-11-27 01:39:47 +0200

    Style modal for desktop

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -9,6 +9,14 @@ export function Modal({ setIsOpen, ...props }: ModalProps) {
   return (
     <div className="modal-container">
       <div className="dark-bg" onClick={() => setIsOpen(false)}></div>
+      <div className="solid-center">
+        <div className="bottom-container">
+          <button className="cancel" onClick={() => setIsOpen(false)}>
+            CANCEL
+          </button>
+          <button className="done">DONE</button>
+        </div>
+      </div>
     </div>
   );
 }
