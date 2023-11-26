904fd5f9c5198da43e87eef41245d27edfc572c2
tamiryup <tamirishaysh@gmail.com>
2023-11-26 23:45:26 +0200

    Move NavBar to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { SongsPage } from "pages/songs.page";
+import { NavBar } from "components/nav-bar/navbar";
 
 function App() {
   return (
     <div className="wrapper">
+      <NavBar />
       <SongsPage />
     </div>
   );
--- src/pages/songs.page.tsx
+++ src/pages/songs.page.tsx
@@ -5,18 +5,15 @@ import { SongBox } from "components/songbox/songbox";
 
 export function SongsPage() {
   return (
-    <>
-      <NavBar />
-      <div className="content-wrapper">
-        <div className="songs">
-          <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
-          <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
-          <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
-          <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
-          <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
-          <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
-        </div>
+    <div className="content-wrapper">
+      <div className="songs">
+        <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
+        <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
+        <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
+        <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
+        <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
+        <SongBox title="Fire and Rain" artist="James Taylor" genre="Chill" />
       </div>
-    </>
+    </div>
   );
 }
